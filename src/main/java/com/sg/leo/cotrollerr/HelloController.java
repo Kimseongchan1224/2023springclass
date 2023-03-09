83f3cda464c6a62d26c2d25eb92bd61cf6a33074
Kimseongchan1224 <konm1224@naver.com>
2023-03-09 14:53:51 +0900

    정적콘텐츠관리

---
+++ src/main/java/com/sg/leo/cotrollerr/HelloController.java
@@ -0,0 +1,19 @@
+package com.sg.leo.cotrollerr;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller 
+public class HelloController {
+	@GetMapping("/html")
+	public String html() {
+		System.out.println("HTML 파일이 요청됨");
+		return "redirect:starthere.html";
+	}
+	
+	@GetMapping("/img")
+	public String img() {
+		System.out.println("IMG 파일이 요청됨");
+		return "redirect:img/sonny.jpg";
+	}
+}
